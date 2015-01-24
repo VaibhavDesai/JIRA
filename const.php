f6c540c7798021f8c00416934bb02f86426d747b
vaibhav <desaivaibhav94@gmail.com>
2015-01-24 14:14:35 +0530

    email credentials added

--- const.php
+++ const.php
@@ -6,4 +6,9 @@ $dbhost = "localhost:3036";
 $dbuser = "root";
 $dbpass = "vai";
 $dbuse = 'jira';
+
+//email
+$email_hostname = '{imap.mail.me.com:993/imap/ssl}INBOX';
+$email_username = 'vaibhavdesai';
+$email_password = 'Rabbit@1';
 ?>
