8203e227c44cb06287c2410a095c3047f0b30f67
vaibhav <desaivaibhav94@gmail.com>
2015-02-05 15:48:22 +0530

    removal of control characters from description of email

--- mailExtractor.php
+++ mailExtractor.php
@@ -1,11 +1,12 @@
 <?php
 include "const.php";
 include "databaseSupport.php";
+
 /* try to connect */
 $inbox = imap_open($email_hostname,$email_username,$email_password) or die('Cannot connect to Gmail: ' . imap_last_error());
 
 /* grab emails from a specific email(this is for testing)*/
-$emails = imap_search($inbox,'FROM "desaivaibhav94@gmail.com"');
+$emails = imap_search($inbox,'ALL UNSEEN');
 
 //$emails = imap_search($inbox,'ALL');
 
@@ -20,18 +21,23 @@ if($emails) {
 	
 	foreach ($emails as $email_number)
 	{
-		$email_number = $emails[0];
+		//$email_number = $emails[0];
+
 		//using headerinfo we can gather the information about the sender.
 		$header = imap_headerinfo($inbox,$email_number);
 		$summary = $header->subject;
 		$body = imap_fetchbody($inbox,$email_number,1);
+
 		if(preg_match("/#[a-zA-Z]+/",$body,$match)&&preg_match("/@[a-zA-Z]+/",$body,$match))
 		{
 		$content = preg_replace("/#[a-zA-Z]+/","",$body);
 		$description = preg_replace("/@[a-zA-Z]+/","",$content);
 		preg_match("/#[a-zA-Z]+/",$body,$issueType);
 		preg_match("/@[a-zA-Z]+/",$body,$project_key);
-		//echo "project key ". substr($project_key[0],1)." </br> summary is $summary </br> description is </br>$description</br>issue type is ". substr($issueType[0],1)."<br>";
+
+		//To remove all the character Control.
+		$description = preg_replace('/[\x00-\x1F\x7F]/', '', $description);
+		echo "project key ". substr($project_key[0],1)." </br> summary is $summary </br> description is </br>$description</br>issue type is ". substr($issueType[0],1)."<br>";
 		stowIssues(substr($project_key[0],1),$summary,$description,substr($issueType[0],1));
 		}
 	}
