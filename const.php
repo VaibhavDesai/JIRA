1ab1feb3568c580cc391ca50db94baf153a41681
vaibhav <desaivaibhav94@gmail.com>
2015-02-05 15:44:17 +0530

    Added new const(base_url)

--- const.php
+++ const.php
@@ -1,14 +1,17 @@
 <?php
-$username ='digibat';
-$password ='Rabbit@1';
-$project_key = 'JIRA';
+$username ='innohabit321';
+$password ='innohabit321';
+$project_key = 'WOIS';
 $dbhost = "localhost:3036";
 $dbuser = "root";
 $dbpass = "vai";
-$dbuse = 'jira';
+$dbname = 'jira';
+
+//atlassian base url
+$base_url = 'https://ravikiran.atlassian.net';
 
 //email
-$email_hostname = '{imap.secureserver.net:993/imap/ssl}INBOX';
-$email_username = 'mailtome@vaibhavjdesai.com';
-$email_password = '********';
+$email_hostname = '{imap.mail.me.com:993/imap/ssl}INBOX';
+$email_username = 'vaibhavdesai';
+$email_password = 'Rabbit@1';
 ?>
