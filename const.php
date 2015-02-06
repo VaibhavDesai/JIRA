0d0f32cb0630bb08e5b673d7744afe4c0df02061
vaibhav <desaivaibhav94@gmail.com>
2015-02-07 00:21:37 +0530

    original

--- const.php
+++ const.php
@@ -4,23 +4,13 @@
 $username ='innohabit321';
 $password ='innohabit321';
 $project_key = 'WOIS';
-<<<<<<< HEAD
-$dbhost = "localhost:3036";
-$dbuser = "root";
-$dbpass = "root";
-$dbname = 'jira';
-
-//atlassian base url
-=======
->>>>>>> 790a5311943a2b138040ef3f92e0d9c323797c90
 $base_url = 'https://ravikiran.atlassian.net';
 
 //db settings.
-$dbhost = ":/cloudsql/spartan-archive-846:db-test";
+$dbhost = "173.194.235.122";
 $dbuser = "root";
 $dbpass = "root";
-$db_tmep = 'jira_temp';
-$db_master = 'woisesdb';
+$dbname = 'jira_temp';
 
 //email
 $email_hostname = '{imap.mail.me.com:993/imap/ssl}INBOX';
