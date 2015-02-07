f1e680fe52d656a26f5909244776930db66430c7
root <root@instance-1.c.spartan-archive-846.internal>
2015-02-07 06:05:47 +0000

    beeped out passwords

--- const.php
+++ const.php
@@ -1,21 +1,21 @@
 <?php
 
 //atlassian settings
-$username ='innohabit321';
-$password ='innohabit321';
-$project_key = 'WOIS';
-$base_url = 'https://ravikiran.atlassian.net';
+$username =******;
+$password =******;
+$project_key = ***;
+$base_url = **********;
 
 //db settings.
 $dbhost = "localhost:3036";
-$dbuser = "root";
-$dbpass = "root";
-$dbname = 'woisesdb';
-$temp_jira_table = "jira_temp";
-$master_table = "capturewoises";
+$dbuser = ****;
+$dbpass = ***;
+$dbname = ****;
+$temp_jira_table = ******;
+$master_table = *********;
 
 //email
-$email_hostname = '{imap.mail.me.com:993/imap/ssl}INBOX';
-$email_username = 'vaibhavdesai';
-$email_password = 'Rabbit@1';
+$email_hostname = '{*********:993/imap/ssl}INBOX';
+$email_username = '*********';
+$email_password = '************';
 ?>
